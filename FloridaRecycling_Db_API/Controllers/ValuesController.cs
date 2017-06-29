7430589a68a630f879c6975132ae87a4c78e1e61
Daniel Jackson <danj210@hotmail.com>
2017-06-29 13:00:37 -0400

    Testing

--- FloridaRecycling_Db_API/Controllers/ValuesController.cs
+++ FloridaRecycling_Db_API/Controllers/ValuesController.cs
@@ -3,17 +3,25 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using FloridaRecycling_Db_API.Services.Repository;
+using FloridaRecycling_Db_API.Models;
 
 namespace FloridaRecycling_Db_API.Controllers
 {
     [Route("api/[controller]")]
     public class ValuesController : Controller
     {
+        private IFloridaRecyclingDbRepository _repository;
+
+        public ValuesController(IFloridaRecyclingDbRepository repository) {
+            _repository = repository;
+        }
         // GET api/values
         [HttpGet]
-        public IEnumerable<string> Get()
+        public async Task<IEnumerable<Metals>> Get()
         {
-            return new string[] { "value1", "value2" };
+            //return new string[] { "value1", "value2" };
+            return await _repository.GetMetalsAsync();
         }
 
         // GET api/values/5
